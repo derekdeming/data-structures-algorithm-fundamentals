d80092e901525cf7aef302821a9f1daa7e1f33d5
Derek <derekdeming17@gmail.com>
2023-11-20 13:49:09 -0500

    general tuple stuff

---
+++ tuples/tuples.py
@@ -0,0 +1,13 @@
+t = ('a', 'b', 'c', 'd', 'e')
+newT = tuple()
+newT2 = tuple('abcde')
+
+
+print(t)
+print(newT)
+print(newT2)
+
+
+# creating a tuple: 
+# time complexity: O(1)
+# space complexity: O(n)
